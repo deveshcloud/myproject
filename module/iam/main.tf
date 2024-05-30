75523b9a2e919a5c9f1849e8fcd91a9c4e2dafa5
deveshcloud <deveshthaware2@gmail.com>
2024-05-30 11:21:38 +0530

    Update main.tf

--- module/iam/main.tf
+++ module/iam/main.tf
@@ -1,6 +1,6 @@
 #create iam ploicy
-resource "aws_iam_policy" "example_policy" {
-  name = "eaxample_policy"
+resource "aws_iam_policy" "example_policy_123" {
+  name = "eaxample_policy_123"
   description = "permission for ec2"
   policy = jsonencode({
   "Version": "2012-10-17",
@@ -15,8 +15,8 @@ resource "aws_iam_policy" "example_policy" {
 }
 
 #create iam role
-resource "aws_iam_role" "example_role" {
-  name = "example_role"
+resource "aws_iam_role" "example_role_jenkins" {
+  name = "example_role_jenkins"
   assume_role_policy = jsonencode({
   "Version": "2012-10-17",
   "Statement": [
@@ -33,22 +33,22 @@ resource "aws_iam_role" "example_role" {
 
 #Attach iam policy to iam role
 resource "aws_iam_policy_attachment" "policy_attach" {
-  name = "example_policy_attachment"
-  roles = [aws_iam_role.example_role.name]
-  policy_arn = aws_iam_policy.example_policy.arn
+  name = "example_policy_attachment_123"
+  roles = [aws_iam_role.example_role_jenkins.name]
+  policy_arn = aws_iam_policy.example_policy_123.arn
 }
 
 #create instance profile using role
-resource "aws_iam_instance_profile" "example_profile" {
-  name = "example_profile"
-  role = aws_iam_role.example_role.name  
+resource "aws_iam_instance_profile" "example_profile_12" {
+  name = "example_profile_12"
+  role = aws_iam_role.example_role_jenkins.name  
 }
 
 #create EC2 instance and attache iam role
 resource "aws_instance" "example_instance" {
   instance_type = var.ec2_instance_type
   ami = var.image_id
-  iam_instance_profile = aws_iam_instance_profile.example_profile.name  
+  iam_instance_profile = aws_iam_instance_profile.example_profile_12.name  
   
   tags = {
     name = "my-instance"
